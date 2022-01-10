f29161d7b7c33269176b1e1157a7a99cfa996b32
andradeviniicius <viniciusdandrade01@gmail.com>
2022-01-10 13:57:04 -0300

    Interação javascript seções

--- Bulma/Portfolio/main.js
+++ Bulma/Portfolio/main.js
@@ -12,26 +12,33 @@ burger.addEventListener('click', () => {
 // const allLi = document.querySelector('li');
 
 // allLi.addEventListener('click', () => {
-    //     window.alert('alo');
-    // });
-    
-    // Navbar pages
-    
-    const mainSectionButton = document.querySelector("#aboutButton")
-    const contactSectionButton = document.querySelector("#contactButton")
-    const mainSection = document.querySelector('#mainSection')
-    const contactSection = document.querySelector('#contactSession')
-    
-    
-    
-    mainSectionButton.addEventListener('click', () => {
-        mainSection.style.display = "block";
-        contactSection.style.display = "none"
-    });    
-    contactSectionButton.addEventListener('click', () => {
-        mainSection.style.display = "none";
-        contactSection.style.display = "block"
-        
-    });    
-    
+//     window.alert('alo');
+// });
+
+// Navbar pages
+
+const mainSectionButton = document.querySelector("#aboutButton")
+const contactSectionButton = document.querySelector("#contactButton")
+const mainSection = document.querySelector('#mainSection')
+const contactSection = document.querySelector('#contactSection')
+const profileContainer = document.querySelector('#profileContainer')
+
+
+
+mainSectionButton.addEventListener('click', () => {
+    mainSection.style.display = "block";
+    contactSection.style.display = "none";
+});
+contactSectionButton.addEventListener('click', () => {
+    mainSection.style.display = "none";
+    contactSection.style.display = "block";
+
+});
+$("#contactButton").click(function() {
+    $("#profileContainer").prependTo($("#profileColumnsDiv"));
+  });
+$("#aboutButton").click(function() {
+    $("#profileContainer").prependTo($("#mainColumnsDiv"));
+  });
+
     // relativo graadual 178
